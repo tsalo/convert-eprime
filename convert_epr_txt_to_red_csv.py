8dcf754587383df718fc272448bb88e606e79fb6
Taylor Salo <tsalo90@gmail.com>
2014-11-05 16:04:39 -0800

    Small refactor of convert_epr_txt_to_red_csv

--- convert_epr_txt_to_red_csv.py
+++ convert_epr_txt_to_red_csv.py
@@ -19,7 +19,6 @@ op = "/home/tsalo/Desktop/FAST/"
 TEXT_FILE = op + "RISE_FMRI_ItemRecoga&bVer1-FASTpilot001-1.txt"
 PAIR_FILE = op + "RISE_FMRI_ItemRecoga&bVer1-FASTpilot001-1.edat2"
 OUT_FILE = op + "FASTpilot001-1_IR.csv"
-WHOLE_TEXT_FILE = '/home/tsalo/Desktop/FAST/stuff.csv'
 TASK = "FAST_RISE_IR"
 
 with open(code_dir + "/headers.pickle") as file_:
@@ -60,121 +59,122 @@ def merge_lists(lists, option):
         return lists
     else:
         merged = lists[0]
-        for iCol in range(len(lists)):
+        for i_col in range(len(lists)):
             if option == "allNull":
-                merged = [lists[iCol][iRow] if lists[iCol][iRow] == "NULL" else
-                          merged[iRow] for iRow in range(len(merged))]
+                merged = [lists[i_col][i_row] if lists[i_col][i_row] == "NULL"
+                          else merged[i_row] for i_row in range(len(merged))]
             elif option == "allElse":
-                merged = [lists[iCol][iRow] if lists[iCol][iRow] != "NULL" else
-                          merged[iRow] for iRow in range(len(merged))]
+                merged = [lists[i_col][i_row] if lists[i_col][i_row] != "NULL"
+                          else merged[i_row] for i_row in range(len(merged))]
         return merged
 
+# Load the text file as a list.
 with open(TEXT_FILE, "r") as text:
     text_list = list(text)
 
 # Remove unicode characters.
-filtered = [stripped(x) for x in text_list]
+filtered_text_list = [stripped(x) for x in text_list]
 
-initIdx = [i for i, x in enumerate(filtered) if x == "*** LogFrame Start ***"]
-endIdx = [i for i, x in enumerate(filtered) if x == "*** LogFrame End ***"]
+# Determine where rows begin and end.
+row_start_idx = [i for i, x in enumerate(filtered_text_list)
+                 if x == "*** LogFrame Start ***"]
+row_end_idx = [i for i, x in enumerate(filtered_text_list)
+               if x == "*** LogFrame End ***"]
 
-if len(initIdx) != len(endIdx) or initIdx[0] >= endIdx[0]:
+if (len(row_start_idx) != len(row_end_idx) or
+        row_start_idx[0] >= row_end_idx[0]):
     raise ValueError("LogFrame Starts and Ends do not match up.")
 
-all_headers = []
-unsplit_list = []
-
 # Find column headers and remove duplicates.
-for iLog in range(len(initIdx)):
-    one_row = filtered[initIdx[iLog]+1:endIdx[iLog]]
-    unsplit_list.append(filtered[initIdx[iLog]+1:endIdx[iLog]])
-    for jCol in range(len(one_row)):
-        splitIdx = one_row[jCol].index(": ")
-        all_headers.append(one_row[jCol][:splitIdx])
+all_headers = []
+data_by_rows = []
+
+for iLog in range(len(row_start_idx)):
+    one_row = filtered_text_list[row_start_idx[iLog]+1:row_end_idx[iLog]]
+    data_by_rows.append(filtered_text_list[row_start_idx[iLog] + 1:
+                                           row_end_idx[iLog]])
+    for j_col in range(len(one_row)):
+        split_header_idx = one_row[j_col].index(": ")
+        all_headers.append(one_row[j_col][:split_header_idx])
 
 unique_headers = list(set(all_headers))
 
 # Preallocate list of lists comprised of NULLs.
-arr = ["NULL"] * (len(initIdx)+1)
-mat = [arr[:] for i in range(len(unique_headers))]
+null_col = ["NULL"] * (len(row_start_idx)+1)
+data_matrix = [null_col[:] for i_col in range(len(unique_headers))]
 
-# Fill list of lists with relevant data from unsplit_list and unique_headers.
-for iCol in range(len(unique_headers)):
-    mat[iCol][0] = unique_headers[iCol]
+# Fill list of lists with relevant data from data_by_rows and unique_headers.
+for i_col in range(len(unique_headers)):
+    data_matrix[i_col][0] = unique_headers[i_col]
 
-for iRow in range(len(initIdx)):
-    for jCol in range(len(unsplit_list[iRow])):
-        splitIdx = unsplit_list[iRow][jCol].index(": ")
+for i_row in range(len(row_start_idx)):
+    for j_col in range(len(data_by_rows[i_row])):
+        split_header_idx = data_by_rows[i_row][j_col].index(": ")
         for kHead in range(len(unique_headers)):
-            if unsplit_list[iRow][jCol][:splitIdx] == unique_headers[kHead]:
-                mat[kHead][iRow+1] = unsplit_list[iRow][jCol][splitIdx+2:]
+            if (data_by_rows[i_row][j_col][:split_header_idx] ==
+                    unique_headers[kHead]):
+                data_matrix[kHead][i_row + 1] = (data_by_rows[i_row][j_col]
+                                                 [split_header_idx + 2:])
 
 # If a column is all NULLs except for the header and one value at the bottom,
 # fill the column up with that bottom value.
-for iCol in range(len(mat)):
-    nullIdx = [i for i, x in enumerate(mat[iCol]) if x != "NULL"]
-    if len(nullIdx) == 2 and (nullIdx[1] == len(mat[iCol])-1
-                              or nullIdx[1] == len(mat[iCol])-2):
-        mat[iCol][1:len(mat[iCol])] = ([mat[iCol][nullIdx[1]]] *
-                                      (len(mat[iCol])-1))
-    elif any([header in mat[iCol][0] for header in fill_block]):
-        for s in range(1, len(nullIdx)):
-            mat[iCol][nullIdx[s-1]+1:nullIdx[s]] = (mat[iCol][nullIdx[s]] *
-                                                    len(range(nullIdx[s-1]+1,
-                                                              nullIdx[s])))
+for i_col in range(len(data_matrix)):
+    null_row_idx = [i for i, x in enumerate(data_matrix[i_col]) if x != "NULL"]
+    if len(null_row_idx) == 2 and (null_row_idx[1] == len(data_matrix[i_col]) - 1 or null_row_idx[1] == len(data_matrix[i_col]) - 2):
+        data_matrix[i_col][1:len(data_matrix[i_col])] = ([data_matrix[i_col][null_row_idx[1]]] * (len(data_matrix[i_col]) - 1))
+    elif any([header in data_matrix[i_col][0] for header in fill_block]):
+        for null_row in range(1, len(null_row_idx)):
+            data_matrix[i_col][null_row_idx[null_row - 1] + 1:null_row_idx[null_row]] = (data_matrix[i_col][null_row_idx[null_row]] * len(range(null_row_idx[null_row - 1] + 1, null_row_idx[null_row])))
 
-    mat[iCol] = mat[iCol][:len(mat[iCol])-2]
+    data_matrix[i_col] = data_matrix[i_col][:len(data_matrix[i_col]) - 2]
 
-# Transpose mat.
-tmat = [[row[col] for row in mat] for col in range(len(mat[0]))]
+# Transpose data_matrix.
+transposed_data_matrix = [[row[col] for row in data_matrix] for col in
+                          range(len(data_matrix[0]))]
 
 # Replace text headers with edat headers (replacement dict). Unnecessary if
 # your processing scripts are built around text files instead of edat files.
-tmat[0] = [replacements.get(item, item) for item in tmat[0]]
+transposed_data_matrix[0] = [replacements.get(item, item) for item in
+                             transposed_data_matrix[0]]
 
-fo = open(WHOLE_TEXT_FILE, 'wb')
-file_ = csv.writer(fo)
-for row in tmat:
-    file_.writerow(row)
-fo.close()
-
-# Pare mat down based on desired headers
+# Pare data_matrix down based on desired headers
 # Create list of columns with relevant headers.
-main_array = [tmat[0].index(hed) for hed in header_list]
+main_array = [transposed_data_matrix[0].index(hed) for hed in header_list]
 
 # Create column from which null index will be created.
 null_col_names = null_cols.get(TASK)
 
 nullCol = [main_array[header_list.index(colName)] for colName in
            null_col_names]
-nullCols = [mat[colNum] for colNum in nullCol]
+nullCols = [data_matrix[colNum] for colNum in nullCol]
 mergedNulls = merge_lists(nullCols, "allNulls")
-null_idx = sorted([iRow for iRow in range(len(mergedNulls)) if
-                   mergedNulls[iRow] == "NULL"], reverse=True)
+null_idx = sorted([i_row for i_row in range(len(mergedNulls)) if
+                   mergedNulls[i_row] == "NULL"], reverse=True)
 
 # Merge any columns that need to be merged.
-mergeColList = merge_cols.get(TASK)
-mergeColNameList = merge_col_names.get(TASK)
-mergedCols = []
-for iMerge in range(len(mergeColNameList)):
-    mergeColNums = [tmat[0].index(hed) for hed in mergeColList[iMerge]]
-    mergeCols = [mat[col] for col in mergeColNums]
-    mergedCols.append(merge_lists(mergeCols, "allElse"))
-    mergedCols[iMerge][0] = mergeColNameList[iMerge]
+merge_col_list = merge_cols.get(TASK)
+merge_col_names_list = merge_col_names.get(TASK)
+merged_cols = []
+for i_merge in range(len(merge_col_names_list)):
+    merge_col_nums = [transposed_data_matrix[0].index(hed) for hed in
+                      merge_col_list[i_merge]]
+    merge_cols = [data_matrix[col] for col in merge_col_nums]
+    merged_cols.append(merge_lists(merge_cols, "allElse"))
+    merged_cols[i_merge][0] = merge_col_names_list[i_merge]
 
-out_struct = [[tmat[iRow][col] for col in main_array] for iRow in
-              range(fn.size(tmat, 0))]
+out_struct = [[transposed_data_matrix[i_row][col] for col in main_array] for
+              i_row in range(fn.size(transposed_data_matrix, 0))]
 
-# Transpose mergedCols
-if len(mergedCols) != 0:
-    tmergedCols = [[row[col] for row in mergedCols] for col in
-                   range(len(mergedCols[0]))]
-    for iRow in range(len(out_struct)):
-        out_struct[iRow] = out_struct[iRow] + tmergedCols[iRow]
+# Transpose merged_cols and append them to out_struct.
+if len(merged_cols) != 0:
+    transposed_merged_cols = [[row[col] for row in merged_cols] for col in
+                              range(len(merged_cols[0]))]
+    for i_row in range(len(out_struct)):
+        out_struct[i_row] = out_struct[i_row] + transposed_merged_cols[i_row]
 
 # Remove all instances of NULL by creating an index of NULL occurrences
 # and removing them from out_struct.
-[out_struct.pop(i) for i in null_idx]
+[out_struct.pop(null_row) for null_row in null_idx]
 
 try:
     fo = open(OUT_FILE, 'wb')
@@ -188,4 +188,4 @@ except IOError:
 finally:
     fo.close()
 
-#print("saved " + OUT_FILE)
+print("saved " + OUT_FILE)
