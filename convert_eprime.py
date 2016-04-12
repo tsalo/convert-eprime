710e30481a90fbe56b4b9208f02481158c20863e
Taylor Salo <tsalo90@gmail.com>
2016-04-11 23:30:27 -0400

    Update convert_eprime.py.

--- convert_eprime.py
+++ convert_eprime.py
@@ -53,39 +53,31 @@ def etext_to_rcsv(in_file, task):
         wholefile.pop(0)
 
     # Create list of columns with relevant headers.
-    header_index = [_try_index(wholefile[0], hed) for hed in header_list if
-                    _try_index(wholefile[0], hed) is not None]
+    header_index = [_try_index(wholefile[0], hed) for hed in header_list if _try_index(wholefile[0], hed) is not None]
 
     # Make empty (zeros) list of lists and fill with relevant data from
     # wholefile.
-    out_arr = [[wholefile[i_row][col] for col in header_index]
-               for i_row in range(fn.size(wholefile, 0))]
+    out_arr = [[wholefile[i_row][col] for col in header_index] for i_row in range(fn.size(wholefile, 0))]
 
     # Either remove all instances of NULL or convert all instances of NULL to
     # NaN.
     if remnulls.get(task):
-        null_index = [list(set([iRow for col in out_arr[iRow] if col == "NULL"]))
-                      for iRow in range(fn.size(out_arr, 0))]
-        null_index = sorted([val for sublist in null_index for val in sublist],
-                             reverse=True)
+        null_index = [list(set([i_row for col in out_arr[i_row] if col == "NULL"])) for i_row in range(fn.size(out_arr, 0))]
+        null_index = sorted([val for sublist in null_index for val in sublist], reverse=True)
         [out_arr.pop(i) for i in null_index]
     else:
-        out_arr = [[word.replace("NULL", "NaN") for word in row]
-                   for row in out_arr]
+        out_arr = [[word.replace("NULL", "NaN") for word in row] for row in out_arr]
 
     # Write out and save csv file.
     out_file = in_file[:len(in_file)-3] + "csv"
     try:
-        fo = open(out_file, "wb")
-        file_ = csv.writer(fo)
-        for row in out_arr:
-            file_.writerow(row)
-
+        with open(out_file, "wb") as fo:
+            file_ = csv.writer(fo)
+            for row in out_arr:
+                file_.writerow(row)
         print("Output file successfully created- {0}".format(out_file))
     except IOError:
         print("Can't open output file- {0}".format(out_file))
-    finally:
-        fo.close()
 
 
 def text_to_csv(text_file, out_file):
@@ -103,10 +95,8 @@ def text_to_csv(text_file, out_file):
     filtered_data = [_strip(row) for row in text_data]
     
     # Determine where rows begin and end.
-    start_index = [i_row for i_row, row in enumerate(filtered_data)
-                   if row == "*** LogFrame Start ***"]
-    end_index = [i_row for i_row, row in enumerate(filtered_data)
-                 if row == "*** LogFrame End ***"]
+    start_index = [i_row for i_row, row in enumerate(filtered_data) if row == "*** LogFrame Start ***"]
+    end_index = [i_row for i_row, row in enumerate(filtered_data) if row == "*** LogFrame End ***"]
     if (len(start_index) != len(end_index) or start_index[0] >= end_index[0]):
         print("Warning: LogFrame Starts and Ends do not match up.")
     n_rows = min(len(start_index), len(end_index))
@@ -119,7 +109,7 @@ def text_to_csv(text_file, out_file):
         one_row = filtered_data[start_index[i_row]+1:end_index[i_row]]
         data_by_rows.append(one_row)
         for j_col in range(len(one_row)):
-            split_header_idx = one_row[j_col].index(": ")
+            split_header_idx = one_row[j_col].index(":")
             all_headers.append(one_row[j_col][:split_header_idx])
     
     unique_headers = list(set(all_headers))
@@ -135,41 +125,34 @@ def text_to_csv(text_file, out_file):
     
     for i_row in range(n_rows):
         for j_col in range(len(data_by_rows[i_row])):
-            split_header_idx = data_by_rows[i_row][j_col].index(": ")
+            split_header_idx = data_by_rows[i_row][j_col].index(":")
             for k_header in range(len(unique_headers)):
-                if (data_by_rows[i_row][j_col][:split_header_idx] ==
-                        unique_headers[k_header]):
-                    data_matrix[k_header][i_row + 1] = (data_by_rows[i_row]
-                                                        [j_col]
-                                                        [split_header_idx+2:])
+                if (data_by_rows[i_row][j_col][:split_header_idx] == unique_headers[k_header]):
+                    data_matrix[k_header][i_row+1] = data_by_rows[i_row][j_col][split_header_idx+1:].lstrip()
     
     # If a column is all NULLs except for the header and one value at the
     # bottom, fill the column up with that bottom value.
     for i_col, col in enumerate(data_matrix):
-        rows_w_vals = [j_cell for j_cell, cell in enumerate(col) if
-                       cell != "NULL"]
+        rows_w_vals = [j_cell for j_cell, cell in enumerate(col) if cell != "NULL"]
         # If the column is full of NULLs (except for the last row and the header), len(row_w_vals) = 2
-        if len(rows_w_vals) == 2 and (rows_w_vals[1] == len(col) - 1):
-            data_matrix[i_col][1:len(col)] = ([col[rows_w_vals[1]]] * (len(col) - 1))
     
-        data_matrix[i_col] = col[:len(col) - 2]
+        if len(rows_w_vals) == 2 and (rows_w_vals[1] == len(col)-1):
+            data_matrix[i_col][1:len(col)] = ([col[rows_w_vals[1]]] * (len(col)-1))
+        data_matrix[i_col] = col[:len(col)-2]
     
     # Transpose data_matrix.
     out_matrix = _transpose(data_matrix)
     
     try:
-        fo = open(out_file, 'wb')
-        file_ = csv.writer(fo)
-        for row in out_matrix:
-            file_.writerow(row)
     
+        with open(out_file, "wb") as fo:
+            file_ = csv.writer(fo)
+            for row in out_matrix:
+                file_.writerow(row)
         print("Output file successfully created- {0}".format(out_file))
     except IOError:
         print("Can't open output file- {0}".format(out_file))
-    finally:
-        fo.close()
     
-    print("Saved {0}".format(out_file))
 
 
 def text_to_rcsv(text_file, edat_file, out_file, task):
@@ -190,10 +173,8 @@ def text_to_rcsv(text_file, edat_file, out_file, task):
     filtered_data = [_strip(row) for row in text_data]
 
     # Determine where rows begin and end.
-    start_index = [i_row for i_row, row in enumerate(filtered_data)
-                   if row == "*** LogFrame Start ***"]
-    end_index = [i_row for i_row, row in enumerate(filtered_data)
-                 if row == "*** LogFrame End ***"]
+    start_index = [i_row for i_row, row in enumerate(filtered_data) if row == "*** LogFrame Start ***"]
+    end_index = [i_row for i_row, row in enumerate(filtered_data) if row == "*** LogFrame End ***"]
     if (len(start_index) != len(end_index) or start_index[0] >= end_index[0]):
         print("Warning: LogFrame Starts and Ends do not match up.")
     n_rows = min(len(start_index), len(end_index))
@@ -206,7 +187,7 @@ def text_to_rcsv(text_file, edat_file, out_file, task):
         one_row = filtered_data[start_index[i_row]+1:end_index[i_row]]
         data_by_rows.append(one_row)
         for j_col in range(len(one_row)):
-            split_header_idx = one_row[j_col].index(": ")
+            split_header_idx = one_row[j_col].index(":")
             all_headers.append(one_row[j_col][:split_header_idx])
 
     unique_headers = list(set(all_headers))
@@ -222,30 +203,26 @@ def text_to_rcsv(text_file, edat_file, out_file, task):
 
     for i_row in range(n_rows):
         for j_col in range(len(data_by_rows[i_row])):
-            split_header_idx = data_by_rows[i_row][j_col].index(": ")
+            split_header_idx = data_by_rows[i_row][j_col].index(":")
             for k_header in range(len(unique_headers)):
-                if (data_by_rows[i_row][j_col][:split_header_idx] ==
-                        unique_headers[k_header]):
-                    data_matrix[k_header][i_row + 1] = (data_by_rows[i_row]
-                                                        [j_col]
-                                                        [split_header_idx+2:])
+                if (data_by_rows[i_row][j_col][:split_header_idx] == unique_headers[k_header]):
+                    data_matrix[k_header][i_row+1] = data_by_rows[i_row][j_col][split_header_idx+1:].lstrip()
 
     # If a column is all NULLs except for the header and one value at the
     # bottom, fill the column up with that bottom value.
     # THIS SECTION NEEDS CLEANUP!
     for i_col, col in enumerate(data_matrix):
-        rows_w_vals = [j_cell for j_cell, cell in enumerate(col) if
-                       cell != "NULL"]
-        if len(rows_w_vals) == 2 and (rows_w_vals[1] == len(col) - 1 or rows_w_vals[1] == len(col) - 2):
-            data_matrix[i_col][1:len(col)] = ([col[rows_w_vals[1]]] * (len(col) - 1))
+        rows_w_vals = [j_cell for j_cell, cell in enumerate(col) if cell != "NULL"]
+        if len(rows_w_vals) == 2 and (rows_w_vals[1] == len(col) - 1 or rows_w_vals[1] == len(col)-2):
+            data_matrix[i_col][1:len(col)] = ([col[rows_w_vals[1]]] * (len(col)-1))
         elif any([header in col[0] for header in fill_block]):
             for null_row in range(1, len(rows_w_vals)):
-                first = rows_w_vals[null_row - 1] + 1
+                first = rows_w_vals[null_row-1] + 1
                 last = rows_w_vals[null_row]
-                n_rows_to_fill = len(range(rows_w_vals[null_row - 1] + 1, rows_w_vals[null_row]))
+                n_rows_to_fill = len(range(rows_w_vals[null_row-1] + 1, rows_w_vals[null_row]))
                 data_matrix[i_col][first:last] = (col[rows_w_vals[null_row]] * n_rows_to_fill)
 
-        data_matrix[i_col] = col[:len(col) - 2]
+        data_matrix[i_col] = col[:len(col)-2]
 
     # Transpose data_matrix.
     t_data_matrix = _transpose(data_matrix)
@@ -253,8 +230,7 @@ def text_to_rcsv(text_file, edat_file, out_file, task):
     # Replace text headers with edat headers (replacement dict). Unnecessary if
     # your processing scripts are built around text files instead of edat
     # files.
-    t_data_matrix[0] = [replacements.get(item, item) for item in
-                        t_data_matrix[0]]
+    t_data_matrix[0] = [replacements.get(item, item) for item in t_data_matrix[0]]
 
     # Pare data_matrix down based on desired headers
     # Create list of columns with relevant headers.
@@ -265,14 +241,12 @@ def text_to_rcsv(text_file, edat_file, out_file, task):
     merge_col_names_list = merge_col_names.get(task)
     merged_data = []
     for i_merge in range(len(merge_col_names_list)):
-        merge_col_nums = [t_data_matrix[0].index(hed) for hed in
-                          columns_to_merge[i_merge]]
+        merge_col_nums = [t_data_matrix[0].index(hed) for hed in columns_to_merge[i_merge]]
         data_to_merge = [data_matrix[col] for col in merge_col_nums]
         merged_data.append(_merge_lists(data_to_merge, "all_else"))
         merged_data[i_merge][0] = merge_col_names_list[i_merge]
 
-    out_matrix = [[t_data_matrix[i_row][col] for col in header_index]
-                  for i_row in range(fn.size(t_data_matrix, 0))]
+    out_matrix = [[t_data_matrix[i_row][col] for col in header_index] for i_row in range(fn.size(t_data_matrix, 0))]
 
     # Transpose merged_data and append them to out_matrix.
     if len(merged_data) != 0:
@@ -284,27 +258,22 @@ def text_to_rcsv(text_file, edat_file, out_file, task):
     # Remove all instances of NULL by creating an index of NULL occurrences
     # and removing them from out_matrix.
     null_column_names = null_cols.get(task)
-    null_column_index = [header_index[header_list.index(column)] for column in
-                         null_column_names]
+    null_column_index = [header_index[header_list.index(column)] for column in null_column_names]
     nulls_to_merge = [data_matrix[col_num] for col_num in null_column_index]
     merged_nulls_list = _merge_lists(nulls_to_merge, "all_null")
-    null_index = sorted([i_row for i_row in range(len(merged_nulls_list)) if
-                         merged_nulls_list[i_row] == "NULL"], reverse=True)
+    null_index = sorted([i_row for i_row in range(len(merged_nulls_list)) if merged_nulls_list[i_row] == "NULL"], reverse=True)
     [out_matrix.pop(null_row) for null_row in null_index]
 
     try:
-        fo = open(out_file, 'wb')
-        file_ = csv.writer(fo)
-        for row in out_matrix:
-            file_.writerow(row)
+        with open(out_file, "wb") as fo:
+            file_ = csv.writer(fo)
+            for row in out_matrix:
+                file_.writerow(row)
 
         print("Output file successfully created- {0}".format(out_file))
     except IOError:
         print("Can't open output file- {0}".format(out_file))
-    finally:
-        fo.close()
 
-    print("Saved {0}".format(out_file))
 
 
 def _det_file_type(in_file):
@@ -384,7 +353,7 @@ if __name__ == "__main__":
                         if (inspect.isfunction(obj) and not name.startswith('_'))]
     
     if function_name not in module_functions:
-        raise IOError("Function " + function_name + " not in convert_eprime.")
+        raise IOError("Function {0} not in convert_eprime.".format(function_name))
     
     function = globals()[function_name]
     n_args = len(inspect.getargspec(function).args)
