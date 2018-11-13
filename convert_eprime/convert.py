34f88a5c1999c638bc8c6ea472e810a8789dceec
ianad <ian.dalton@ucf.edu>
2018-11-13 17:04:03 -0500

    Add contingency for crash-dump metadata

--- convert_eprime/convert.py
+++ convert_eprime/convert.py
@@ -201,7 +201,12 @@ def _text_to_df(text_file):
     start_index = [i for i, row in enumerate(filtered_data) if row == '*** LogFrame Start ***']
     end_index = [i for i, row in enumerate(filtered_data) if row == '*** LogFrame End ***']
     if len(start_index) != len(end_index) or start_index[0] >= end_index[0]:
+    # In cases of an experiment crash, the final LogFrame is never written, and the experiment metadata
+    # (Subject, VersionNumber, etc.) isn't collected by the indices above. We can manually include the
+    # metadata-containing Header Frame to collect these data from a partial-run crash dump.
         print('Warning: LogFrame Starts and Ends do not match up.')
+        start_index += [i for i,row in enumerate(filtered_data if row == '*** Header Start ***']
+        end_index += [i for i,row in enumerate(filtered_data if row == '*** Header_End ***']
     n_rows = min(len(start_index), len(end_index))
 
     # Find column headers and remove duplicates.
